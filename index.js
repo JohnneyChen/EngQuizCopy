0391712c3aabf5db78b9a53158555596f43f3c24
Johnney Chen <mzchenca@gmail.com>
2021-03-09 19:01:01 -0700

    changed port to process.env

--- index.js
+++ index.js
@@ -105,6 +105,8 @@ app.use((err, req, res, next) => {
     res.status(err.statusCode || 500).render('error', { message, stack })
 })
 
-app.listen(3000, () => {
-    console.log('Server listening on port 3000')
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
 })
